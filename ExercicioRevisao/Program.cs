2946f4f306056b4f5469a724ad7d3758e6386bb1
Matheus Lima <limalmatheus7@gmail.com>
2024-08-07 17:26:36 -0300

    exercicios de revisao

---
+++ ExercicioRevisao/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nota");
+int Nota = int.Parse(Console.ReadLine());
+
+if (Nota == 6){
+    Console.WriteLine("recuperacao");
+}
+
+else if(Nota <= 5){
+    Console.WriteLine("Reprovado");
+}
+
+else{
+    Console.WriteLine("PASSOU!!!");
+}
+
